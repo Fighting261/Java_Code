4e8516366414ffc1c94f12c71dba3ebcc5261dd8
Fighting261 <1227873949@qq.com>
2020-04-12 13:41:45 +0800

    HelloWorld

--- 20200407/HelloWorld.java
+++ 20200407/HelloWorld.java
@@ -6,11 +6,6 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		int a = 0;
-		System.out.println(a);
-		int max = Integer.MIN_VALUE - 1;
-		System.out.println(max);
-		int min = Integer.MAX_VALUE + 1;
-		System.out.println(min);
+		System.out.println("HelloWorld");
 	}
 }
